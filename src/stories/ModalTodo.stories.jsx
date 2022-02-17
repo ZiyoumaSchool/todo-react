fc70db74945619d4d589a33eeb695791b0a5322b
RoosveltK <roosvelt.kenne.9817@facsciences-uy1.cm>
2022-02-17 16:59:55 +0100

    ok is good

--- src/stories/ModalTodo.stories.jsx
+++ src/stories/ModalTodo.stories.jsx
@@ -11,8 +11,21 @@ export default {
 
 const Template = (args) => <ModalTodo {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  primary: true,
-  label: "Button",
+export const EditModal = Template.bind({});
+EditModal.args = {
+  modalContent: {
+    title: "Modification",
+    bntLabel: "Modifier",
+  },
+  actionBtn: null,
+};
+
+export const DeleteModal = Template.bind({});
+DeleteModal.args = {
+  modalContent: {
+    title: "Suppression de la todo",
+    content: "Voulez vous vraiment supprimer ce todo?",
+    bntLabel: "Oui",
+  },
+  actionBtn: null,
 };
